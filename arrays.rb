b6adab51b4f095a57bf08cb1bc388830c9fa0fda
Mostafa Sakr <msakr3@gmail.com>
2022-08-03 10:53:24 -0600

    Correct put function to puts; moved comment on pop method above the method; add push method, shift method, and unshift method to different arrays with comments for each;use puts the arrays to check if methods applied correctly

--- arrays.rb
+++ arrays.rb
@@ -1,8 +1,21 @@
-put ""
+puts""
 food = ["ma7shi", "molo5eyya", "sushi", "shawarma"]
 fibonacci = [0, 1, 1, 2]
 floatato = [0.5, 2.3, 4.6, -1.2]
 booleans = [true, false, false,false]
-put""
+puts""
 
-booleans.pop(2) #remove elements 2 and 3 in booleans array as there are only true and false
+#remove elements 2 and 3 in booleans array as there are only true and false
+booleans.pop(2)
+
+#add 5 more elements to the fibonacci array using push
+fibonacci.push(3, 5, 8, 13, 21)
+
+#remove elements 0 and 1 from floatato array
+floatato.shift(2)
+
+#added two new elements (foods) at the beginning of food array
+food.unshift("okra", "eggplant salad")
+
+#put arrays to check work!
+puts "", food, fibonacci, floatato, booleans, "" #empty quotation marks in an attempt to have space before and after in terminal
